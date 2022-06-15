f5a9268ccf681877d9217c69028881f12c4e4c7a
amrbashir <amr.bashir2015@gmail.com>
2022-06-15 17:28:19 +0200

    update to latest tauri

--- template-vanilla/src-tauri/src/main.rs
+++ template-vanilla/src-tauri/src/main.rs
@@ -9,7 +9,9 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let context = tauri::generate_context!();
     tauri::Builder::default()
+        .menu(tauri::Menu::os_default(&context.package_info().name))
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
