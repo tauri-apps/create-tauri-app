7cdffe08760d0b4dd686d14a994f388a4fbdb982
amrbashir <amr.bashir2015@gmail.com>
2022-07-19 16:44:30 +0200

    update to vite@3 and add vite browser list, closes #84

--- fragments/fragment-svelte-ts/vite.config.ts
+++ fragments/fragment-svelte-ts/vite.config.ts
@@ -18,10 +18,14 @@ export default defineConfig({
         // to make use of `TAURI_DEBUG` and other env variables
         // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
         envPrefix: ['VITE_', 'TAURI_'],
-        // don't minify for debug builds
-        minify: !process.env.TAURI_DEBUG && 'esbuild',
-        // produce sourcemaps for debug builds
-        sourcemap: !!process.env.TAURI_DEBUG
+        build: {
+           // Tauri supports es2021
+          target: ['es2021', 'chrome100', 'safari13'],
+          // don't minify for debug builds
+          minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+          // produce sourcemaps for debug builds
+          sourcemap: !!process.env.TAURI_DEBUG
+        }
       }
     : {})
 })
