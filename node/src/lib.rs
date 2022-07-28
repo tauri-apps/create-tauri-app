0f84eaf40b659bdda98078f877b1ffb329d01571
amrbashir <amr.bashir2015@gmail.com>
2022-07-28 16:22:39 +0200

    install with frozen-lockfile

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,8 +1,3 @@
-use napi::{
-  threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
-  Error, JsFunction, Result, Status,
-};
-
 #[napi_derive::napi]
 fn run(args: Vec<String>, bin_name: Option<String>) {
   create_tauri_app::run(args, bin_name);
