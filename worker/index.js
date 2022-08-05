0c45d3e76a7125af8f377bd8443ad773cda7b6fc
amrbashir <amr.bashir2015@gmail.com>
2022-08-05 21:57:55 +0200

    use `/ps` instead of `/ps1`

--- worker/index.js
+++ worker/index.js
@@ -1,12 +1,12 @@
 import sh from "../create-tauri-app.sh";
-import ps1 from "../create-tauri-app.ps1";
+import ps from "../create-tauri-app.ps1";
 
 const filesList = `
     <h1> Index of create.tauri.app </h1>
     <hr>
         <pre>
 <a href="/sh">create-tauri-app.sh</a>
-<a href="/ps1">create-tauri-app.ps1</a>
+<a href="/ps">create-tauri-app.ps1</a>
         </pre>
     <hr>
 `;
@@ -21,8 +21,8 @@ export default {
       });
     }
 
-    if (pathname.startsWith("/ps1")) {
-      return new Response(ps1, {
+    if (pathname.startsWith("/ps")) {
+      return new Response(ps, {
         headers: { "Content-Type": "text/plain" },
       });
     }
