05802eef90d22126075b605b42922dea7f2916d0
amrbashir <amr.bashir2015@gmail.com>
2022-08-06 15:48:15 +0200

    fmt

--- packages/worker/index.js
+++ packages/worker/index.js
@@ -1,18 +1,24 @@
 import sh from "./scripts/create-tauri-app.sh";
 import ps from "./scripts/create-tauri-app.ps1";
-import index from "./index.html"
+import index from "./index.html";
 
 export default {
   async fetch(request) {
     const { pathname } = new URL(request.url);
 
-    if (pathname.startsWith("/sh") || pathname.startsWith("/create-tauri-app.sh")) {
+    if (
+      pathname.startsWith("/sh") ||
+      pathname.startsWith("/create-tauri-app.sh")
+    ) {
       return new Response(sh, {
         headers: { "Content-Type": "text/plain" },
       });
     }
 
-    if (pathname.startsWith("/ps") || pathname.startsWith("/create-tauri-app.ps1")) {
+    if (
+      pathname.startsWith("/ps") ||
+      pathname.startsWith("/create-tauri-app.ps1")
+    ) {
       return new Response(ps, {
         headers: { "Content-Type": "text/plain" },
       });
