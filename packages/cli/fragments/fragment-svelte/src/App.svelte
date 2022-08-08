d9dc34f8c552bd00e41d2d1ad0f45599146edb93
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-08-08 14:22:50 +0100

    Update logo hover colors

--- packages/cli/fragments/fragment-svelte/src/App.svelte
+++ packages/cli/fragments/fragment-svelte/src/App.svelte
@@ -27,10 +27,10 @@
 
 <style>
   .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+    filter: drop-shadow(0 0 2em #747bff);
   }
 
   .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+    filter: drop-shadow(0 0 2em #ff3e00);
   }
 </style>
