b1824a25debf285980b4b31b5d7ddf1655f05176
amrbashir <amr.bashir2015@gmail.com>
2022-08-21 23:50:10 +0200

    change react-ts fragment port to 1420

--- packages/cli/fragments/fragment-react-ts/vite.config.ts
+++ packages/cli/fragments/fragment-react-ts/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         clearScreen: false,
         // tauri expects a fixed port, fail if that port is not available
         server: {
-          port: 7000,
+          port: 1420,
           strictPort: true,
         },
         // to make use of `TAURI_DEBUG` and other env variables
