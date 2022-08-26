195abc0362e400cf754d8733d880493b972a0338
amrbashir <amr.bashir2015@gmail.com>
2022-08-26 14:47:35 +0200

    chore: fmt

--- packages/cli/node/src/lib.rs
+++ packages/cli/node/src/lib.rs
@@ -5,5 +5,5 @@
 #[allow(unused)]
 #[napi_derive::napi]
 fn run(args: Vec<String>, bin_name: Option<String>) {
-  create_tauri_app::run(args, bin_name);
+    create_tauri_app::run(args, bin_name);
 }
