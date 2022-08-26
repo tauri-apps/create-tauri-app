7498648efb592b959e04e8422f6de973f14e70af
amrbashir <amr.bashir2015@gmail.com>
2022-08-26 19:27:42 +0200

    NO it is not fun

---
+++ .scripts/generate-templates-matrix.js
@@ -0,0 +1,52 @@
+const nodeJsTemplates = [
+    "svelte",
+    "svelte-ts",
+    "vue",
+    "vue-ts",
+    "solid",
+    "solid-ts",
+    "react",
+    "react-ts",
+    "vanilla",
+    "next",
+    "next-ts",
+];
+
+const matrix = [
+    {
+        manager: "pnpm",
+        install_cmd: "pnpm install",
+        run_cmd: "pnpm",
+        templates: nodeJsTemplates,
+    },
+    {
+        manager: "yarn",
+        install_cmd: "yarn",
+        run_cmd: "yarn",
+        templates: nodeJsTemplates,
+    },
+    {
+        manager: "npm",
+        install_cmd: "npm install",
+        run_cmd: "npm run",
+        templates: nodeJsTemplates,
+    },
+    {
+        manager: "cargo",
+        install_cmd: "",
+        run_cmd: "cargo",
+        templates: ["vanilla", "yew"],
+    },
+];
+
+
+const outMatrix = []
+matrix.map(e => e.templates).forEach((t, i) => {
+    let { templates, ...managerInfo } = matrix[i];
+    outMatrix.push({
+        template: t,
+        ...managerInfo,
+    })
+})
+
+console.log(JSON.stringify(outMatrix));
