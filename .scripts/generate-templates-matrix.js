3f6d1c2e8023040defa1680d1f485e40d592ac7f
amrbashir <amr.bashir2015@gmail.com>
2022-08-27 13:29:29 +0200

    test all templates if templates-test.yml is changed

--- .scripts/generate-templates-matrix.js
+++ .scripts/generate-templates-matrix.js
@@ -52,7 +52,8 @@ matrix
           (e) =>
             e.includes(`fragment-${t}`) ||
             e.includes("packages/cli/src") ||
-            e.includes("packages/cli/Cargo.toml")
+            e.includes("packages/cli/Cargo.toml") ||
+            e.includes(".github/workflows/templates-test.yml")
         )
       ) {
         outMatrix.push({
