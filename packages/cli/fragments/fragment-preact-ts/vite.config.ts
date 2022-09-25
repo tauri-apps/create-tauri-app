49982c942975ee0edb34706f157f50b1ce1741be
BiliGoldenWater <2439577268@qq.com>
2022-09-25 21:31:56 +0800

    chore: fix typo in vite config files (#190)
    * chore: typo
    
    * add change file
    
    * remove change file https://github.com/tauri-apps/create-tauri-app/pull/190/files#r979400427

--- packages/cli/fragments/fragment-preact-ts/vite.config.ts
+++ packages/cli/fragments/fragment-preact-ts/vite.config.ts
@@ -5,7 +5,7 @@ import preact from "@preact/preset-vite";
 export default defineConfig({
   plugins: [preact()],
 
-  // Vite optons tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // tauri expects a fixed port, fail if that port is not available
--- packages/cli/fragments/fragment-react/vite.config.js
+++ packages/cli/fragments/fragment-react/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  // Vite optons tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // tauri expects a fixed port, fail if that port is not available
