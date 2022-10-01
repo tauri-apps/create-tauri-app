bd5c1850e08bf411fc98abde230a5880e7384e01
Mel Massadian <melmassadian@gmail.com>
2022-10-01 15:19:17 +0200

    style: ✨ pnpm format

--- packages/cli/fragments/fragment-svelte-kit-ts/vite.config.js
+++ packages/cli/fragments/fragment-svelte-kit-ts/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
