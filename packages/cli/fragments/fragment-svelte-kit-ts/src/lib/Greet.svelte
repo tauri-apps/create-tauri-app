4e4fef7f9eeac8f114f41b21e161641bb7574a06
Mel Massadian <melmassadian@gmail.com>
2022-10-01 16:00:23 +0200

    fix: ✨ match the other boilerplates

--- packages/cli/fragments/fragment-svelte-kit-ts/src/lib/Greet.svelte
+++ packages/cli/fragments/fragment-svelte-kit-ts/src/lib/Greet.svelte
@@ -1,22 +1,19 @@
-<script>
-    import { invoke } from "@tauri-apps/api/tauri"
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
 
-    let name = "";
-    let greetMsg = ""
+  let name = "";
+  let greetMsg = "";
 
-    async function greet(){
-      // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-      greetMsg = await invoke("greet", { name })
-    }
-  </script>
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
 
-  <div>
-    <div class="row">
-      <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-      <button on:click={greet}>
-        Greet
-      </button>
-    </div>
-    <p>{greetMsg}</p>
+<div>
+  <div class="row">
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button on:click={greet}> Greet </button>
   </div>
-
+  <p>{greetMsg}</p>
+</div>
