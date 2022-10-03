6b37f3e52d837efb08cb20acbf8daf2f60492fc3
Mel Massadian <melmassadian@gmail.com>
2022-10-03 21:23:50 +0200

    fix: 🎨 remove template comments

--- packages/cli/fragments/fragment-svelte-kit-ts/src/app.d.ts
+++ packages/cli/fragments/fragment-svelte-kit-ts/src/app.d.ts
@@ -2,8 +2,5 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-  // interface Locals {}
-  // interface PageData {}
-  // interface Error {}
-  // interface Platform {}
+
 }
