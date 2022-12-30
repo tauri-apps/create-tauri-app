f1d5ea377415bffd9a0e411145399f1f198fb658
Lukas <65358837+lukthony@users.noreply.github.com>
2022-12-30 15:30:42 -0600

    fix: add typescript support in .svelte files
    enable and add preprocessor for sveltekit and svelte respectively
    
    (fix #276)

--- packages/cli/fragments/fragment-svelte-kit-ts/svelte.config.js
+++ packages/cli/fragments/fragment-svelte-kit-ts/svelte.config.js
@@ -1,7 +1,9 @@
 import staticAdapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: staticAdapter(),
   },
