3ea6acf54b1f9517043121b0ce0bacf8910f175c
amrbashir <amr.bashir2015@gmail.com>
2023-01-02 06:03:53 +0200

    feat(templates): update vite conf to match latest tauri docs

---
+++ .changes/vite-conf.md
@@ -0,0 +1,6 @@
+---
+"create-tauri-app": "patch"
+"create-tauri-app-js": "patch"
+---
+
+Update `vite.conf.json` target option to match the latest tauri docs.
--- packages/cli/fragments/fragment-svelte-ts/vite.config.ts
+++ packages/cli/fragments/fragment-svelte-ts/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
-    target: ["es2021", "chrome100", "safari13"],
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
     // don't minify for debug builds
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
