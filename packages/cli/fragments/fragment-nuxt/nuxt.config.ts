4a382254164883c1f64743bd78512e8f361e9f7a
Simon Hyll <hyllsimon@gmail.com>
2023-01-15 08:52:45 +0100

    fix: disabled telemetry

--- packages/cli/fragments/fragment-nuxt/nuxt.config.ts
+++ packages/cli/fragments/fragment-nuxt/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // Tauri does not support SSR
+  telemetry: false,
   srcDir: "src",
   css: ["~/assets/css/style.css"],
 });
