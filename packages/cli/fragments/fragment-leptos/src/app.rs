81c9d9e4974e8e9a0f76ad560ea4a8b6b896b1a4
Pikhosh <73311467+pikhosh@users.noreply.github.com>
2023-03-08 18:30:49 +0330

    🛠️ Refactor(Fragments/Leptos): Remove `web-sys`
    Leptos v0.2 doesn't re-export the `Event` type from the `web-sys` so in order to remove the `web-sys` we need to remove the explicit type from the `update_name` closure
    
    Co-Authored-By: Amr Bashir <amr.bashir2015@gmail.com>

--- packages/cli/fragments/fragment-leptos/src/app.rs
+++ packages/cli/fragments/fragment-leptos/src/app.rs
@@ -1,4 +1,4 @@
-use web_sys::{Event, SubmitEvent};
+use leptos::leptos_dom::ev::{SubmitEvent};
 use leptos::*;
 use serde::{Deserialize, Serialize};
 use serde_wasm_bindgen::to_value;
@@ -20,7 +20,7 @@ pub fn App(cx: Scope) -> impl IntoView {
     let (name, set_name) = create_signal(cx, String::new());
     let (greet_msg, set_greet_msg) = create_signal(cx, String::new());
 
-    let update_name = move |ev: Event| {
+    let update_name = move |ev| {
         let v = event_target_value(&ev);
         set_name.set(v);
     };
