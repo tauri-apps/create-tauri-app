daaf01c1091e7d940c674ad9f27c5c515a13df56
James McDonald <38434825+mcdonaldjc@users.noreply.github.com>
2023-07-06 13:42:21 -0500

    feat: make greet() functional from a web browser
    By default, this template only works when running `npm run tauri`. For scalability, adding a quick check and making it function with just TypeScript may be a good idea.

--- packages/cli/fragments/fragment-vanilla-ts/src/main.ts
+++ packages/cli/fragments/fragment-vanilla-ts/src/main.ts
@@ -3,12 +3,21 @@ import { invoke } from "@tauri-apps/api/tauri";
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
 
+function isTauri() {
+  return typeof window !== "undefined" && "__TAURI__" in window;
+}
+
 async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
+  if (greetMsgEl && greetInputEl && greetInputEl.value.length > 0) {
+    if (isTauri()) {
+      // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+      greetMsgEl.textContent = await invoke("greet", {
+        name: greetInputEl.value,
+      });
+      return;
+    }
+
+    greetMsgEl.textContent = `Hello, ${greetInputEl.value}! You've been greeted from TypeScript!`;
   }
 }
 
