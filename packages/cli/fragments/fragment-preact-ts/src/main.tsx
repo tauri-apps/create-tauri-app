e7a9992db2de0147e2773e5fc3a4d361ad94ed7d
Ryan Christian <rchristian@ryanchristian.dev>
2023-07-10 15:31:19 -0500

    chore: Satisfy TS

--- packages/cli/fragments/fragment-preact-ts/src/main.tsx
+++ packages/cli/fragments/fragment-preact-ts/src/main.tsx
@@ -2,4 +2,4 @@ import { render } from "preact";
 import App from "./App";
 import "./styles.css";
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root")!);
