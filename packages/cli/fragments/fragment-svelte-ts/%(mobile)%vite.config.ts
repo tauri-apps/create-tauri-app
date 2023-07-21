7eda1555c175f0a00a83065c2549151b3af425d4
Amr Bashir <amr.bashir2015@gmail.com>
2023-07-21 03:14:44 +0300

    chore: remove svelte unused import

--- packages/cli/fragments/fragment-svelte-ts/%(mobile)%vite.config.ts
+++ packages/cli/fragments/fragment-svelte-ts/%(mobile)%vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
 import { internalIpV4 } from "internal-ip";
 
 const mobile =
--- packages/cli/fragments/fragment-svelte-ts/%(stable-alpha)%vite.config.ts
+++ packages/cli/fragments/fragment-svelte-ts/%(stable-alpha)%vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
