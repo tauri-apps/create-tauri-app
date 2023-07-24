cb62dcc3ca8adb3a580c9187bfe27068f3e7dfa7
Amr Bashir <amr.bashir2015@gmail.com>
2023-07-24 19:34:35 +0300

    feat: cleanup vite config file

--- packages/cli/fragments/fragment-react/%(stable-alpha)%vite.config.js
+++ packages/cli/fragments/fragment-react/%(stable-alpha)%vite.config.js
@@ -6,22 +6,15 @@ export default defineConfig(async () => ({
   plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  // prevent vite from obscuring rust errors
+  //
+  // 1. prevent vite from obscuring rust errors
   clearScreen: false,
-  // tauri expects a fixed port, fail if that port is not available
+  // 2. tauri expects a fixed port, fail if that port is not available
   server: {
     port: 1420,
     strictPort: true,
   },
-  // to make use of `TAURI_DEBUG` and other env variables
+  // 3. to make use of `TAURI_DEBUG` and other env variables
   // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
   envPrefix: ["VITE_", "TAURI_"],
-  build: {
-    // Tauri supports es2021
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
 }));
