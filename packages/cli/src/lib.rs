d78b6ea78c31897824fb725c8b7450c180c58aca
Lucas Mülling <lmulling@proton.me>
2023-10-30 17:25:41 +0300

    fix: handle SIGINT on user prompt, closes (#509) (#511)
    * fix: handle SIGINT on user prompt (#509)
    
    * fix: initialize SIGINT handler in lib.rs, fix error messages (#509)
    
    * fix: downgrade ctrlc to 3.3.1 (#509)
    
    * Update packages/cli/Cargo.toml

--- packages/cli/src/lib.rs
+++ packages/cli/src/lib.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
 
+use anyhow::Context;
 use dialoguer::{Confirm, Input, Select};
 use std::{ffi::OsString, fs, process::exit};
 
@@ -40,6 +41,7 @@ where
     I: IntoIterator<Item = A>,
     A: Into<OsString> + Clone,
 {
+    ctrlc::set_handler(move || eprint!("\x1b[?25h")).expect("fail to set SIGINT handler");
     if let Err(e) = try_run(args, bin_name, detected_manager) {
         eprintln!("{BOLD}{RED}error{RESET}: {e:#}");
         exit(1);
@@ -81,19 +83,23 @@ where
 
     // Project name used for the project directory name
     // and if valid, it will also be used in Cargo.toml, Package.json ...etc
-    let project_name = project_name.unwrap_or_else(|| {
-        if skip {
-            defaults.project_name.unwrap()
-        } else {
-            Input::<String>::with_theme(&ColorfulTheme::default())
-                .with_prompt("Project name")
-                .default("tauri-app".into())
-                .interact_text()
-                .unwrap()
-                .trim()
-                .to_string()
+    let project_name = match project_name {
+        Some(name) => name,
+        None => {
+            if skip {
+                defaults
+                    .project_name
+                    .context("default project_name not set")?
+            } else {
+                Input::<String>::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Project name")
+                    .default("tauri-app".into())
+                    .interact_text()?
+                    .trim()
+                    .into()
+            }
         }
-    });
+    };
 
     let target_dir = cwd.join(&project_name);
 
@@ -191,8 +197,7 @@ where
                 .with_prompt("Choose which language to use for your frontend")
                 .items(&categories)
                 .default(0)
-                .interact()
-                .unwrap();
+                .interact()?;
             Some(categories[index])
         }
     } else {
@@ -201,86 +206,91 @@ where
 
     // Package manager which will be used for rendering the template
     // and the after-render instructions
-    let pkg_manager = manager.unwrap_or_else(|| {
-        if skip {
-            defaults.manager.unwrap()
-        } else {
-            let category = category.unwrap();
-
-            let mut managers = category.package_managers().to_owned();
-            // sort managers so the auto-detected package manager is selected first
-            managers.sort_by(|a, b| {
-                detected_manager
-                    .map(|p| p == *b)
-                    .unwrap_or(false)
-                    .cmp(&detected_manager.map(|p| p == *a).unwrap_or(false))
-            });
-
-            // If only one package manager is detected, skip prompt
-            if managers.len() == 1 {
-                managers[0]
+    let pkg_manager = match manager {
+        Some(manager) => manager,
+        None => {
+            if skip {
+                defaults.manager.context("default manager not set")?
             } else {
-                let index = Select::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Choose your package manager")
-                    .items(&managers)
-                    .default(0)
-                    .interact()
-                    .unwrap();
-                managers[index]
+                let category = category.context("category shouldn't be None at this point")?;
+
+                let mut managers = category.package_managers().to_owned();
+                // sort managers so the auto-detected package manager is selected first
+                managers.sort_by(|a, b| {
+                    detected_manager
+                        .map(|p| p == *b)
+                        .unwrap_or(false)
+                        .cmp(&detected_manager.map(|p| p == *a).unwrap_or(false))
+                });
+                // If only one package manager is detected, skip prompt
+                if managers.len() == 1 {
+                    managers[0]
+                } else {
+                    let index = Select::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Choose your package manager")
+                        .items(&managers)
+                        .default(0)
+                        .interact()?;
+                    managers[index]
+                }
             }
         }
-    });
+    };
 
     let templates_no_flavors = pkg_manager.templates_no_flavors();
 
     // Template to render
-    let template = template.unwrap_or_else(|| {
-        if skip {
-            defaults.template.unwrap()
-        } else {
-            let index = Select::with_theme(&ColorfulTheme::default())
-                .with_prompt("Choose your UI template")
-                .items(
-                    &templates_no_flavors
-                        .iter()
-                        .map(|t| t.select_text())
-                        .collect::<Vec<_>>(),
-                )
-                .default(0)
-                .interact()
-                .unwrap();
-            let template = templates_no_flavors[index];
-
-            // Prompt for flavors if the template has more than one flavor
-            let flavors = template.flavors(pkg_manager);
-            if let Some(flavors) = flavors {
-                let index = Select::with_theme(&ColorfulTheme::default())
-                    .with_prompt("Choose your UI flavor")
-                    .items(flavors)
-                    .default(0)
-                    .interact()
-                    .unwrap();
-                template.from_flavor(flavors[index])
+    let template = match template {
+        Some(template) => template,
+        None => {
+            if skip {
+                defaults.template.context("default template not set")?
             } else {
-                template
+                let index = Select::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Choose your UI template")
+                    .items(
+                        &templates_no_flavors
+                            .iter()
+                            .map(|t| t.select_text())
+                            .collect::<Vec<_>>(),
+                    )
+                    .default(0)
+                    .interact()?;
+
+                let template = templates_no_flavors[index];
+
+                // Prompt for flavors if the template has more than one flavor
+                let flavors = template.flavors(pkg_manager);
+                if let Some(flavors) = flavors {
+                    let index = Select::with_theme(&ColorfulTheme::default())
+                        .with_prompt("Choose your UI flavor")
+                        .items(flavors)
+                        .default(0)
+                        .interact()?;
+                    template.from_flavor(flavors[index])
+                } else {
+                    template
+                }
             }
         }
-    });
+    };
 
     // Prompt for wether to bootstrap a mobile-friendly tauri project
     // This should only be prompted if `--alpha` is used on the command line and `--mobile` wasn't.
     // TODO: remove this limitation once tauri@v2 is stable
-    let mobile = mobile.unwrap_or_else(|| {
-        if skip || !alpha {
-            defaults.mobile.unwrap()
-        } else {
-            Confirm::with_theme(&ColorfulTheme::default())
-                .with_prompt("Would you like to setup the project for mobile as well?")
-                .default(false)
-                .interact()
-                .unwrap()
+    let mobile = match mobile {
+        Some(mobile) => mobile,
+        None => {
+            if skip || !alpha {
+                defaults.mobile.context("default mobile not set")?
+            } else {
+                Confirm::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Would you like to setup the project for mobile as well?")
+                    .default(false)
+                    .interact()?
+            }
         }
-    });
+    };
 
     // If the package manager and the template are specified on the command line
     // then almost all prompts are skipped so we need to make sure that the combination
