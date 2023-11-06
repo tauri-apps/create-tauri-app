fdf060b8d81f21b56c3e585aa49ebad27a5c44b0
amrbashir <amr.bashir2015@gmail.com>
2023-11-06 21:50:25 +0200

    update versions

--- packages/cli/fragments/fragment-vanilla-ts/%vite.config.ts
+++ packages/cli/fragments/fragment-vanilla-ts/%vite.config.ts
@@ -23,8 +23,5 @@ export default defineConfig(async () => ({
           port: 1421,
         }
       : undefined,{% endif %}
-  },
-  // 3. to make use of `TAURI_DEBUG` and other env variables
-  // https://tauri.app/v1/api/config#buildconfig.beforedevcommand
-  envPrefix: ["VITE_", "TAURI_"],
+  }
 }));
