8030ffaf4858736d46894cd94eb5b75708662168
Pièrre <77776198+PierreV23@users.noreply.github.com>
2024-03-25 16:42:48 +0100

    Add `console_error_panic_hook` to Leptos template.

--- templates/template-leptos/src/main.rs
+++ templates/template-leptos/src/main.rs
@@ -2,8 +2,10 @@ mod app;
 
 use app::*;
 use leptos::*;
+use console_error_panic_hook;
 
 fn main() {
+    console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! {
             <App/>
