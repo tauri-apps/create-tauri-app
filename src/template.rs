66649b1040f2e5bcb9a6dbb126dceeef132ba01d
Aerafal <jimmy.mll@hotmail.com>
2024-04-09 19:45:54 +0200

    feat: add pascal case project name in template data and rename all occurences in the blazor template

--- src/template.rs
+++ src/template.rs
@@ -292,6 +292,10 @@ impl<'a> Template {
             ("beta", beta_str.clone()),
             ("mobile", mobile.to_string()),
             ("project_name", project_name.to_string()),
+            (
+                "project_name_pascal_case",
+                Self::transform_to_pascal_case(project_name.to_string()),
+            ),
             ("package_name", package_name.to_string()),
             (
                 "before_dev_command",
@@ -437,5 +441,23 @@ impl<'a> Template {
         }
 
         Ok(())
+    }
+
+    fn transform_to_pascal_case(s: String) -> String {
+        let mut result = String::new();
+        let mut capitalize_next = false;
+        for (s, c) in s.chars().enumerate() {
+            if s == 0 {
+                result.push(c.to_ascii_uppercase());
+            } else if c == '_' {
+                capitalize_next = true;
+            } else if capitalize_next {
+                result.push(c.to_ascii_uppercase());
+                capitalize_next = false;
+            } else {
+                result.push(c);
+            }
+        }
+        result
     }
 }
