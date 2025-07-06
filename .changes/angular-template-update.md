8d74756fd04029c6d9e506934adb790f784268b3
Josef Bredreck <13408112+JosefBredereck@users.noreply.github.com>
2025-07-06 18:29:51 +0200

    feat: update angular template to latest version

---
+++ .changes/angular-template-update.md
@@ -0,0 +1,6 @@
+---
+"create-tauri-app": patch
+"create-tauri-app-js": patch
+---
+
+updated angular template to v20
--- templates/template-angular/src/app/app.config.ts
+++ templates/template-angular/src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from "@angular/core";
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
