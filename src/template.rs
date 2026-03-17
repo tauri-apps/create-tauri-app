df1da0910e54325fa67f2598164c588388bc0281
katayama8000 <tattu.0310@gmail.com>
2026-03-17 22:12:49 +0900

    feat: streamline process import handling for Deno in Vite config templates

--- src/template.rs
+++ src/template.rs
@@ -359,7 +359,6 @@ impl Template {
             ),
             ("lib_name", lib_name),
             ("styles", styles),
-            ("deno", (pkg_manager == PackageManager::Deno).to_string()),
         ]
         .into();
 
